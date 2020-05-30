ef7db4eea6e5eb9d7c9210b5e0a183d9752651c8
Matheus-Mazieiro <Matheus_mazieiro@hotmail.com>
2020-05-30 19:54:03 -0300

    Bugado

---
+++ Assets/Scripts/Gambiarra.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gambiarra : MonoBehaviour
+{
+    public Movement player;
+
+    public void EnableMov(string state)
+    {
+        if (state == "false")
+        {
+            player.myCC.enabled = false;
+            player.enabled = false;
+        }
+        else if (state == "true")
+        {
+            player.myCC.enabled = true;
+            player.enabled = true;
+        }
+    }
+}
--- Assets/Scripts/Player/AnimController.cs
+++ Assets/Scripts/Player/AnimController.cs
@@ -20,13 +20,25 @@ public class AnimController : MonoBehaviour
     void Update()
     {
         Vector2 velocity = new Vector2(myCC.velocity.x, myCC.velocity.z);
-        anim.SetFloat("velocity", velocity.magnitude);
+        if (myCC.enabled)
+        {
+            anim.SetFloat("velocity", velocity.magnitude);
 
-        anim.SetBool("agachar", player.m_isCrouching);
+            anim.SetBool("agachar", player.m_isCrouching);
 
-        anim.SetBool("correr", player.m_isRunning);
+            anim.SetBool("correr", player.m_isRunning);
 
-        anim.SetBool("chao", myCC.isGrounded);
+            anim.SetBool("chao", myCC.isGrounded);
+        }else
+        {
+            anim.SetFloat("velocity", 0);
+
+            anim.SetBool("agachar", false);
+
+            anim.SetBool("correr", false);
+
+            anim.SetBool("chao", true);
+        }
     }
 
     public void SetJumpTrigger()
