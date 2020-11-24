a31599b6bb1dd40560824902b0919a121b28d849
Matheus Mazieiro <Matheus_mazieiro@hotmail.com>
2020-11-24 02:12:09 -0300

    Epilogue and prologue pt 2

---
+++ Assets/Scripts/Ultility/Epilogue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class Epilogue : MonoBehaviour
+{
+    [SerializeField] UnityEvent[] action;
+    [SerializeField] float[] delay;
+    [SerializeField] CinemachineVirtualCamera dollyCam;
+    int idx = 0;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1f);
+        EndGameMethod();
+    }
+
+    void EndGameMethod()
+    {
+        StartCoroutine(EndGame());
+    }
+    IEnumerator EndGame()
+    {
+        for (int i = 0; i < action.Length; i++)
+        {
+            Debug.Log("<b> [Epilogue]: </b>" + i);
+            action[i].Invoke();
+            yield return new WaitForSeconds(delay[i]);
+        }
+    }
+
+    public void MoveDollyCam()
+    {
+        CinemachineTrackedDolly dCam = dollyCam.GetCinemachineComponent<CinemachineTrackedDolly>();
+        Debug.Log("<b> [Epilogue]: </b>" + dCam);
+        DOTween.To(() => dCam.m_PathPosition, x => dCam.m_PathPosition = x, 2, 20).SetOptions(false);
+    }
+
+}
---
+++ Assets/Scripts/Ultility/Prelogue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Prelogue : MonoBehaviour
+{
+    [SerializeField] UnityEvent action;
+    [SerializeField] UnityEvent cmAction;
+    [SerializeField] Image blackScreen;
+    [SerializeField] Text yearsAfter;
+    [SerializeField] AudioSource heartBeat;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(13);
+        DOTween.To(() => blackScreen.color, x => blackScreen.color = x, new Color(0, 0, 0, 0), 7);
+        yield return new WaitForSeconds(2);
+        action.Invoke();
+        cmAction.Invoke();
+        heartBeat.Play();
+        yield return new WaitForSeconds(11);
+        DOTween.To(() => blackScreen.color, x => blackScreen.color = x, new Color(0, 0, 0, 1), 2);
+        yield return new WaitForSeconds(3);
+        DOTween.To(() => yearsAfter.color, x => yearsAfter.color = x, new Color(0.81132080f, 0.8113208f, 0.8113208f, 1), 3);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
+
+    }
+}
